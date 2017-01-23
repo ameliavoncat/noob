fe7702c00529eca3c1a0dfd1c9bd0611247c0499
anasauce <anasauceda1843@gmail.com>
2017-01-23 14:36:18 -0800

    Setting up Postgres DB [resolve #3]  - installed PG, nodemon, dontenv dependencies  - setup .env file  - setup initial config and database file structures

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const pg = require('pg')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
