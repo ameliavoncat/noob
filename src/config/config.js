b4e5715ea511abead6f6d1c5afce2c056dcc3848
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-01-24 14:22:16 -0800

    cows

--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,10 @@ const pg = require('pg');
 const fs = require('fs')
 const connectionString = process.env.DATABASE_URL
 
-if ( fs.existSync('.env') ) {
-  require('dotenv').config()
-}
-
 const env = () =>
- process.env.NODE_ENV || 'development'
+  process.env.NODE_ENV || 'development'
 
-if ( fs.existsSync('.env') ){
+if ( fs.existSync('.env') ) {
   require('dotenv').config()
 }
 
