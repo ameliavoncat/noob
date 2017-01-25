962b7d89ad526a143c1749959b88d4935426a022
Francisco Rocha <frankiefabuloso@gmail.com>
2017-01-25 15:17:44 -0800

    Added react to webpack build, changed browser's file system (#30)
    * Added react to webpack build, changed browser's file system
    
    * fixing merge conflicts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const path = require('path')
 const pg = require('pg')
-
+const publicPath = path.resolve(__dirname, './public')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Server is running <3 <3')
+  res.sendFile(publicPath+'/index.html')
 })
 
 module.exports = router
