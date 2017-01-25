a57f52033b556bb5c49cbed7bd42d67abb72f431
Francisco Rocha <frankiefabuloso@gmail.com>
2017-01-25 08:57:55 -0800

    adds webpack config (#21)

--- src/app.js
+++ src/app.js
@@ -10,13 +10,12 @@ import auth from './init/auth'
 
 const app = express()
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 auth( app )
 
