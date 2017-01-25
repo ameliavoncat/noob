ba819d824da6bea43a7e77c007316a5c9d134a8a
thamaranth <thamaranth@gmail.com>
2017-01-24 16:29:17 -0800

    added development/production environments. Refactored bin/www

--- src/init/auth.js
+++ src/init/auth.js
@@ -1,18 +1,22 @@
 import { addUserToRequestFromJWT } from '@learnersguild/idm-jwt-auth/lib/middlewares'
+import { parseConfig } from '../config/config'
 
-const URL = 'http://idm.learnersguild.dev'
-const REDIRECT = encodeURIComponent('http://noob.learnersguild.dev')
-
-const init = app => {
+const initialize = app => {
+  if ( !process.env.JWT_PUBLIC_KEY ) {
+    throw new Error(`You do not have a JWT_PUBLIC_KEY in your .env. Please add it.`)
+  }
   app.use( addUserToRequestFromJWT )
   app.use( ensureUserLoggedIn )
 }
 
 const ensureUserLoggedIn = (req, res, next) => {
+  const config = parseConfig(),
+    redirect = encodeURIComponent(config.redirect_url)
+
   if ( !req.user ) {
-    return res.redirect(`${URL}/sign-in?redirect=${REDIRECT}`)
+    return res.redirect(`${config.idm_url}/sign-in?redirect=${redirect}`)
   }
   next()
 }
 
-export default init
+export default initialize
