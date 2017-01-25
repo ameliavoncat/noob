a4bb10803ff0c7b8dd6f8d50540379c34dabd3b3
FrankieFabuloso <frocha@ucsc.edu>
2017-01-25 14:25:20 -0800

    Added react to webpack build, changed browser's file system

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ const rootDir = __dirname
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const jsonLoader = require('json-loader');
-// const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-//   template: rootDir + '/src/browser/index.html',
-//   filename: 'index.html',
-//   inject: 'body',
-//   minify: {
-//     removeComments: true,
-//     collapseWhitespace: true,
-//     removeRedundantAttributes: true,
-//     useShortDoctype: true,
-//     removeEmptyAttributes: true,
-//     removeStyleLinkTypeAttributes: true,
-//     keepClosingSlash: true,
-//     minifyJS: true,
-//     minifyCSS: true,
-//     minifyURLs: true
-//   }
-// })
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: rootDir + '/src/browser/index.html',
+  filename: 'index.html',
+  inject: 'body',
+  minify: {
+    removeComments: true,
+    collapseWhitespace: true,
+    removeRedundantAttributes: true,
+    useShortDoctype: true,
+    removeEmptyAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    keepClosingSlash: true,
+    minifyJS: true,
+    minifyCSS: true,
+    minifyURLs: true
+  }
+})
 
 module.exports = {
   entry: ['./src/browser/main.js'],
@@ -31,6 +31,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: `${rootDir}/src/public/dist`
-  }
-  // plugins: [HTMLWebpackPluginConfig]
+  },
+  plugins: [HTMLWebpackPluginConfig]
 }
