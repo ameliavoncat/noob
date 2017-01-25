77230ac97b98e4907ef8fbabcbf038edc656202f
thamaranth <thamaranth@gmail.com>
2017-01-25 15:35:10 -0800

    merging script

--- tests/queries_tests/task_test.js
+++ tests/queries_tests/task_test.js
@@ -1,9 +1,9 @@
 import chai, { expect } from 'chai'
-import task from '../../database/queries/task'
+import task from '../../src/database/queries/task'
 
 describe('task', () => {
   const fakeData = {
-   template: 'noob',
+   template_task_id: 1,
    user_id: 1,
    is_complete: false,
    due_date: '02-01-2017',
