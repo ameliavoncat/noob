40358d476dd6f793439306b5dda9e2d2f6108e79
anasauce <anasauceda1843@gmail.com>
2017-01-27 09:15:05 -0800

    add crud functions for users ( & noobs) [resolves #26] (#36)
    * untracked bundle.js
    
    * created reusable knex queries
    
    * accidentally tracked /dist folder
    
    * created intitial crud for users/noobs -WIP
    
    * WIP writing noob and mentor knex queries
    
    * add queries for mentor, user, noob, adminn
    
    * add crud functions for users ( & noobs) [resolves #26]
     - adds defaults to DB for created_at and updated_at in users migration
     - create CRUD queries for noob table
     - update query for users table
     - create additional queries for adim and mentor roles
     - test coverage for users and noob queries
     - added .env.test to gitignore
    
    * remove "Noob" from all functions in noob.js, imports utitlies as an object
    
    * assign imported objects to a variable, removes "User" from query methods

---
+++ src/database/queries/noob.js
@@ -0,0 +1,55 @@
+import knex from '../knex'
+
+import * as utilities from './utilities'
+
+const create = attributes =>
+  utilities.createRecord('noob', attributes).then( noob => noob )
+
+const findByHandle = github_handle =>
+  utilities.findRecord('noob', 'github_handle', github_handle).then(noob => noob)
+
+const updateByHandle = (github_handle, attributes) =>
+  utilities.updateRecord('noob', 'github_handle', github_handle, attributes).then(noob => noob)
+
+const deleteByHandle = github_handle =>
+  utilities.deleteRecord('noob', 'github_handle', github_handle)
+
+const getAllByStartDate = start_date =>
+  utilities.findAllWhere('noob', 'start_date', start_date).then(noob => noob)
+
+const findAll = () =>
+  utilities.findAll('noob').then(noob => noob)
+
+const graduate = github_handle => {
+  return knex.transaction((t) => {
+    return knex('noob')
+      .transacting(t)
+      .where('github_handle', github_handle)
+      .then(result => {
+        result[0].role = 'mentor'
+        delete result[0].mentor_id
+        delete result[0].start_date
+        return knex('users')
+          .transacting(t)
+          .insert(result[0])
+          .then(_ => {
+            return knex('noob')
+            .where('github_handle', github_handle)
+            .del()
+          })
+      })
+      .then(t.commit)
+      .catch(t.rollback)
+  })
+}
+
+
+export {
+  create,
+  findByHandle,
+  updateByHandle,
+  deleteByHandle,
+  getAllByStartDate,
+  findAll,
+  graduate
+}
---
+++ src/tests/queries_tests/user_test.js
@@ -0,0 +1,96 @@
+import chai, { expect } from 'chai'
+import knex from '../../database/knex'
+import * as user from '../../database/queries/users'
+
+describe('user', () => {
+  const newUser = [
+    {
+      full_name: "Ugly Face",
+      github_handle: "Trump_Butt",
+      role: 'mentor',
+      email: 'fart_monster@hemorroid.butt',
+    },
+    {
+      full_name: "Nice Person",
+      github_handle: "Drake_fabulous",
+      role: 'mentor',
+      email: 'youfancynow@ovo.com',
+    },
+    {
+      full_name: "Nancy Drew",
+      github_handle: "mystery_solved",
+      role: 'mentor',
+      email: 'isolvedthemystery@detective.org',
+    }
+  ]
+
+  it('exists', () => {
+    expect(user).to.be.a('object')
+  })
+
+  describe('create', () => {
+    it('inserts user into table', () => {
+      return user.create(newUser[0]).then(user => {
+        expect(user.full_name).to.equal('Ugly Face')
+      })
+    })
+  })
+
+  describe('findByHandle', () => {
+    it('return the all of the user data related to github_handle', () => {
+      return user.create(newUser[1])
+      .then(_ => {
+        user.findByHandle('Drake_fabulous')
+        .then(user => {
+          expect(user.full_name).to.equal('Nice Person')
+        })
+      })
+    })
+  })
+
+  describe('updateByHandle', () => {
+    it('updates the name of a user associated with github_handle', () => {
+      return user.create(newUser[2])
+      .then(_ => {
+        user.updateByHandle('mystery_solved', {full_name: 'Veronica Mars'})
+        .then(user => {
+          expect(user.full_name).to.equal('Veronica Mars')
+        })
+      })
+    })
+  })
+
+  describe('deleteByHandle', () => {
+    it('removes user associated with github_handle from database', () => {
+      return user.deleteByHandle('Trump_Butt').then(_ => {
+        user.findByHandle('Trump_Butt')
+        .then(empty => {
+          expect(empty).to.be.equal( undefined )
+        })
+      })
+    })
+  })
+
+  describe('findByRole', () => {
+    it('finds the role associated with user', () => {
+      return user.findByRole('mentor').then(user => {
+        for(let mentor of user){
+          expect(mentor.role).to.be.equal('mentor')
+        }
+      })
+    })
+  })
+
+  describe('findAll', () => {
+    it('finds all the users in the users table', () => {
+      return user.findAll('mentor').then(user => {
+        let count = 0
+        for(let entry of user){
+          count += 1
+        }
+        expect(user.length).to.be.equal(count)
+      })
+    })
+  })
+
+})
