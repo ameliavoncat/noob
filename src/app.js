9e3ed97326ad5d2adae0c28b5a53b909e2b0a1fb
FrankieFabuloso <frocha@ucsc.edu>
2017-01-27 15:25:41 -0800

    hot reloading only happens in development

--- src/app.js
+++ src/app.js
@@ -5,18 +5,20 @@ import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import webpack from 'webpack'
 import config from '../webpack.config'
+import { getEnv }from './config/config'
 
 
 import auth from './init/auth'
 
 const app = express()
 const compiler = webpack(config);
+if(getEnv() === 'development') {
+  app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath
+  }));
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath
-}));
-console.log('------->IN APP JSrhfb:')
-app.use(require('webpack-hot-middleware')(compiler));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
