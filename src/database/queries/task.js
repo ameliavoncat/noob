1efc1757bf448a0339ddefd265c4551583d839c6
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-01-26 17:59:19 -0800

    Refactored exports

--- src/database/queries/task.js
+++ src/database/queries/task.js
@@ -1,31 +1,22 @@
 import knex from '../knex'
 import * as _ from './utilities'
 
-const addTask = attributes =>
+const add = attributes =>
   _.createRecord( 'task', attributes )
 
-const getAllTasks = () =>
+const getAll = () =>
   _.findAll( 'task' )
 
-const getTaskBy = ( column, data ) =>
+const getBy = ( column, data ) =>
   _.findAllWhere( 'task', column, data )
 
-const updateTask = ( column, data, attributes ) =>
+const update = ( column, data, attributes ) =>
   _.updateRecord( 'task', column, data, attributes )
 
-const expungeTask = ( column, data ) =>
+const expunge = ( column, data ) =>
   _.deleteRecord( 'task', column, data )
 
-const deleteAllTasks = () =>
+const deleteAll = () =>
   _.deleteAll( 'task' )
 
-const task = {
-  add: addTask,
-  getAll: getAllTasks,
-  getBy: getTaskBy,
-  update: updateTask,
-  expunge: expungeTask,
-  deleteAll: deleteAllTasks
-}
-
-export default task
+export { add, getAll, getBy, update, expunge, deleteAll }
--- tests/queries_tests/task_test.js
+++ tests/queries_tests/task_test.js
@@ -1,5 +1,5 @@
 import chai, { expect } from 'chai'
-import task from '../../src/database/queries/task'
+import * as task from '../../src/database/queries/task'
 
 describe('task', () => {
 
